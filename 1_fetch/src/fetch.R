b91750db4850ecc5ee300f5801d2ab56754396db
Snyder <asnyder@usgs.gov>
2021-09-15 12:49:04 -0400

    update fetch.R file functions to match _targets.R functions

--- 1_fetch/src/fetch.R
+++ 1_fetch/src/fetch.R
@@ -1,17 +1,10 @@
-library(sbtools)
+#' @param out_filepath string file path under which to save data locally (in out dir)
 
-
-#' @param sb_id character ID corresponding to ScienceBase item
-#' @param src_filename string file name attached to SB item that you wish to download
-#' @param local_filename string file name under which to save data locally (in out dir)
-
-download_sbdata <- function(sb_id, src_filename, local_filename) {
+download_data <- function(out_filepath) {
   # Create out data directory
-  project_output_dir =  file.path('1_fetch', 'out')
+  project_output_dir =  dirname(out_filepath)
   dir.create(project_output_dir, showWarnings = FALSE)
   # Get the data from ScienceBase
-  out_file <- file.path(project_output_dir, local_filename)
-  item_file_download(sb_id, names = src_filename, destinations = out_file, overwrite_file = TRUE)
-}
-
-download_sbdata(sb_id = '5d925066e4b0c4f70d0d0599', src_filename = 'me_RMSE.csv', local_filename = 'model_RMSEs.csv')
+  item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv', destinations = out_filepath, overwrite_file = TRUE)
+  return(out_filepath)
+  }
